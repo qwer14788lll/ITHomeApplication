e609a3ba36fc68528b39b8fbd0abfc8fb37c19f5
奉嘉 <qwer14788lll@163.com>
2020-11-03 10:01:22 +0800

    项目初始化

---
+++ app/src/main/java/com/example/ithomeapplication/News.kt
@@ -0,0 +1,18 @@
+package com.example.ithomeapplication
+
+/**
+ * title：新闻标题
+ * time：发布时间
+ * comment：评论数
+ * img：新闻配图
+ * editor：主编
+ * content：新闻内容
+ */
+class News(
+    val title: String,
+    val time: String,
+    val comment: String,
+    val img: Int,
+    val editor: String,
+    val content: String
+)
